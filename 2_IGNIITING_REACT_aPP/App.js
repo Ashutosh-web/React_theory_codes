3bb9c9528cef1c96984de8f7d3404f83e03257ba
Ashutosh Kumar <ak2417942@gmail.com>
2023-01-02 23:10:40 +0530

    creating folders for 2 and 3 lec

---
+++ 2_IGNIITING_REACT_aPP/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/*
+ * HMR : Hot module replacementT
+ * File watcher algorithm : c++
+ * Super fast build production
+ * Caching while development
+ * Compression
+ * Compatible with older version of browser
+ * Image optimization
+ * HTTPS on dev
+ * It manages the port no : If we are developing the two or more projects concurrently
+ * It uses consistent hashing algorithm
+ * Zero Configuration bundler,optimizer and minifying
+ * Transitive dependencies : sub dependencies of main dependendencies
+ * Polyphill
+ * It creates a server for us
+ * Tree Shaking : Removing un-wanted code
+ */
+
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Namste World!"
+);
+
+const heading1 = React.createElement(
+  "h2",
+  {
+    id: "title-desc",
+  },
+  "By Ashutosh Kumar"
+);
+
+const container = React.createElement("div", null, [heading, heading1]);
+
+//heading is a react element which is basically a object and React.createElement is method that creates a DOM object
+console.log(heading);
+
+//ReactDOM.createRoot is creating a root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//parsing a react element inside a root element
+//root.render(heading);
+root.render(container);
---
+++ 3_LAying_The_foundation/README.md
@@ -0,0 +1,8 @@
+**for making bold content**
+_for making itaclic content_
+
+# The largest heading
+
+## Seconf lasrgest heading
+
+### Smallest heading
