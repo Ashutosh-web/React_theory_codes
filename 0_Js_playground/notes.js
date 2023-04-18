97c94d4be3944288375858a666f696da767783ae
anoyash <ak2417942@gmail.com>
2023-04-18 11:14:18 +0530

    notes updated till lec - 9

--- 0_Js_playground/notes.js
+++ 0_Js_playground/notes.js
@@ -10,7 +10,25 @@
 
 4. window object : it gets created everytime even in a single empty js file.
 
+5. Temporal Dead Zone : It is the time from which let variables are declared till the time of its execution. Till that time we cannot access the variables.
+So, we will get uncaught reference error : Cannot access variables before its initialization.
 
+6. Uncaught reference error : Cannot access variables before its initialization Vs Uncaught reference error : not defined. Both these errors are different.
+
+7. let and const declarations are also hoiseted. But they are in different memory space , not in global space. It Can not be used, since it is in temporal dead zone. Cannot 
+be access from window global object.
+
+8. Const declaration is more strict : 
+8.1 : Missing intialization in const declaration
+8.2 : Uncaught type Error : Assignment to const variable
+
+9. With var , we can re - declare a variable
+var a = 10;
+var a = 100;
+
+10. with let , we cannot re - declare
+let a = 1;
+let a = 100;   // this is not correct , not allowed
 
 
 */
