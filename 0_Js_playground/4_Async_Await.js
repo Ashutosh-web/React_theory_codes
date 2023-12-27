90154478a97fb53f1c95aa9a0867814f66aeb5b8
anoyash <ak2417942@gmail.com>
2023-12-27 11:42:31 +0530

    async await update

--- 0_Js_playground/4_Async_Await.js
+++ 0_Js_playground/4_Async_Await.js
@@ -7,8 +7,10 @@
 2. What is await ? : 
     * await Can be used inside a async function
     * It can also be used before a call to a function that returns a promise
-    * This makes the code wait at that point unitil the promise is settled
+    * This makes the code wait at that point unitil the promise is settled , it does not stops the js engine execution , for that particular period it will go out of the 
+    * call and once the promise is resolved , the promise handler again comes to the call stack.
 3. How async await works behind the scenes ? :
+    * Need to understand what happens behind the scenes when we use async keyword before the functio 
 
 4. Example of async await :
     * See below code sample
@@ -19,13 +21,13 @@
 
  const p1 = new Promise( (resolve, reject) => {
     setTimeout( () => {
-        resolve("p1 promise is resolved");
+        resolve("p1 promise is resolved after 1 sec");
     },1000);
  });
 
  const p2 = new Promise( (resolve, reject) => {
     setTimeout( ()=>{
-        resolve("p2 promise is resolved");
+        resolve("p2 promise is resolved after 5 seconds");
     },5000);
  });
 
@@ -40,7 +42,7 @@
 
 // console.log(value);
 
-async function getData () {
+async function promiseHandler () {
     
     //const val = p1.then( (result) => console.log(result));
     console.log("Hello World");
@@ -52,7 +54,57 @@ async function getData () {
     console.log("I am here promise 2");
     console.log(val2);
 }
+ 
+promiseHandler();
 
-getData();
+console.log("I am here executed before the promises are resolved");
 
 
+/* below example i variable is not declared again and again */
+// counter = () =>{
+//     let i; 
+//     for( i=1;i<=5;i++){
+//         setTimeout( () => {
+//             console.log(i);
+//         },i*1000);
+//     }
+// }
+
+// counter();
+
+/* below example i variable is  declared again and again */
+// counter = () =>{
+//     for(let i=1;i<=5;i++){
+//         setTimeout( () => {
+//             console.log(i);
+//         },i*1000);
+//     }
+// }
+
+//counter();
+
+const API_URL = "https://api.github.com/users/anoyash";
+
+// using async in arrow functions looks like this
+githubCaller = async () => {
+    console.log("Inside github call");
+
+    try {
+        const data = await fetch(API_URL);
+        console.log(data);
+        console.log(typeof(data));
+        const jsonData = await data.json();
+        console.log(jsonData);
+        console.log(typeof(jsonData));
+    } catch( error ) { 
+        console.log(error);
+    }
+} 
+
+githubCaller();
+
+// another way of handling error is, because githubCaller is asynchronous function and 
+// it will return a promise because we are using async in it.
+// githubCaller().catch((error) => {
+//     console.log(error);
+// });
